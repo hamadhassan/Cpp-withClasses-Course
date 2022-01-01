8d28796d6bdd44db8a5bf34aab4caea5ba3673f2
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-01 19:50:55 +0500

    Day 01 C++ Course

---
+++ HelloWorld.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    cout << "Hello World" << endl;
+    cout << "Muhammad Hammad Hassan " << endl;
+    cout << "Student of Computer Science at UET Lahore (Main Campus)" << endl;
+}
