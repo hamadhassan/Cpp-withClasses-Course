d7f6b309450e020f58957c3d9344fc57899c03bf
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-02 20:08:34 +0500

    Week 01 C++ Course

---
+++ Expression.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int pb1 = 0;
+    pb1 = 2 / 1 + 5;
+    cout << "Problem 1 result is:" << pb1 << endl;
+    cout << "**********" << endl;
+
+    float pb2 = 0.123;
+    pb2 = 3 / 4 + (2 - 1);
+    cout << "Problem 2 result is:" << pb2 << endl;
+    cout << "**********" << endl;
+
+    int pb3 = 7 + (600 - 100) * 8;
+    cout << "Problem 3 result is:" << pb3 << endl;
+    cout << "**********" << endl;
+
+    int pb4 = 500 * 400 / 4 + 10;
+    cout << "Problem 3 result is:" << pb4 << endl;
+    cout << "**********" << endl;
+
+    int pb5 = 18 / 2 * 18 - 1;
+    cout << "Problem 3 result is:" << pb5 << endl;
+    cout << "**********" << endl;
+}
