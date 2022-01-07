c2a83d152f419f09ec906ebcbbc9e504d951692b
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-07 20:52:01 +0500

    Week 02 C++ Course

---
+++ TwoNumberSum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main()
+{ // Sum two numbers
+    int num1, num2;
+    cout << "Enter Fist number:";
+    cin >> num1;
+    cout << "Enter Second number:";
+    cin >> num2;
+    num1 = num1 + num2;
+    cout << "The sum of two number:" << num1;
+}
