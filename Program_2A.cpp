fbf24a0c7d26b608383f11f980da3eaeffedaeb8
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-08 15:33:06 +0500

    Week 02 C++ Course

---
+++ Program_2A.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    // Multipe table for the number upto five places
+    int num;
+    cout << "Enter any number:";
+    cin >> num;
+    cout << "The multiples are:" << num << " " << num + num << " " << num + num + num << " " << num + num + num + num << " " << num + num + num + num + num;
+}
