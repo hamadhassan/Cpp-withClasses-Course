a6149647d35a7ad140a8386615f77468e60ab74e
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-09 15:34:15 +0500

    Week 02 C++ Course

---
+++ Program_3.cpp
@@ -0,0 +1,45 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout << "Enter first number:";
+cin>> num1;
+cout << "Enter second number:";
+cin>> num2;
+
+cout<<num1;
+
+cout<<" "<<num2;
+
+num1=num1+num2;
+cout<<" "<< num1;
+
+num1=num1+num2;
+cout<<" "<<num1;
+
+num2=num1+num2;
+cout<<" "<< num2;
+
+num1=num2+num1;
+cout<<" "<< num1;
+
+num2=num2+num1;
+cout<<" "<< num2;
+
+num1=num2+num1;
+cout<<" "<< num1;
+
+num2=num2+num1;
+cout<<" "<< num2;
+
+num1=num2+num1;
+cout<<" "<< num1;
+
+num2=num2+num1;
+cout<<" "<< num2;
+
+num1=num2+num1;
+cout<<" "<< num1;
+
+}
