066f3c14f4a8cd6d3e849255a0b53456d44afed6
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-13 16:54:21 +0500

    Week 02 C++ Course

---
+++ Program_2B.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num1,num2;
+cout << "Enter first number:";
+cin>> num1;
+cout << "Enter second number:";
+cin>> num2;
+cout<<"The multiples of first number are:"<< num1<< " "<< num1+num1 << " "<< num1+num1+num1<<" "<<num1+num1+num1+num1<<" "<< num1+num1+num1+num1+num1;
+cout<<endl<<"The multiples of second number are:"<< num2<< " "<< num2+num2 << " "<< num2+num2+num2<<" "<<num2+num2+num2+num2<<" "<< num2+num2+num2+num2+num2;
+
+}
