215d27a1198777fe48e26ca4d1f655a750b07f54
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-14 17:56:42 +0500

    Week 02 C++ Course

---
+++ Problem_1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+// Write a C++ program to print half pyramid using *
+int main()
+{
+    cout << "*" << endl;
+    cout << "**" << endl;
+    cout << "***" << endl;
+    cout << "****" << endl;
+    cout << "*****";
+}
