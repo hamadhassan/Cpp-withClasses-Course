6a909be0809d3744413f593d6e1c9cd044a7bd27
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-21 18:34:07 +0500

    Week 04 C++ Course

---
+++ Challenge_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+string wd1,wd2;
+cout << "Enter the first word : ";
+cin>> wd1;
+cout << "Enter the seconf word : ";
+cin>> wd2;
+if(wd1==wd2)
+{
+cout<< "Yes,the both word is same.";
+}
+else 
+{
+cout<< "No,the both word is not same";
+}
+}
