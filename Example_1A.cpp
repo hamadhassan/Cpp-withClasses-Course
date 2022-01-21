e42b4fe753117d6dc27b373b6fb5b2bd8cd8b151
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-21 18:35:46 +0500

    Week 04 C++ Course

---
+++ Example_1A.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int cost;
+cout<<"Enter the the cost of the dress : ";
+cin>> cost;
+if(cost>1500)
+{
+cout<<"I will buy that dress..."<<endl;;
+}
+cout <<"Program End.";
+}
