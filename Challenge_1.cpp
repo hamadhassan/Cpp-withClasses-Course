95b68118e27d191f258415e307b5d8fa7070c264
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-21 18:32:52 +0500

    Week 04 C++ Course

---
+++ Challenge_1.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+int n;
+cout<< "Enter a integer number : ";
+cin>> n;
+if(n%2==0)
+{
+cout<<"The number is even.";
+}
+else 
+{
+cout <<"The number is odd";
+}
+}
