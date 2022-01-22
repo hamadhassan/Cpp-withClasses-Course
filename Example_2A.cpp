c54138e7f15dbe9cf148dbd40e3d29ee090c4a8b
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-22 21:18:12 +0500

    Week 04 C++ Course

---
+++ Example_2A.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num;
+cout<<"Is you are going to university... "<<endl;
+cout<<"Type 1 if you going to university." << endl;
+cout<<"Type 0 if you not going to universty.";
+cout<<endl<<"Enter your choice : ";
+cin>> num;
+if(num==1)
+{
+cout << "I am also going to university."<<endl;
+}
+cout <<"Program End.";
+
+}
