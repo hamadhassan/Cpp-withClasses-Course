e4d3743dd2f039e4a506d52ba0e2b78d54d22f02
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-23 21:00:26 +0500

    Week 04 C++ Course

---
+++ Example_3D.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+string name;
+cout<<"Enter your good name: ";
+cin>> name;
+if(name=="ali")
+{
+cout<<"Welcome Ali"<<endl;
+}
+else
+{
+cout<<"Try Again."<<endl;
+}
+cout<<"Program End.";
+}
