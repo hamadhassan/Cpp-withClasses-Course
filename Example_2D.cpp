1bbe39f3df993e87d273f5fc3f018ddeb90aaa38
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-23 20:56:28 +0500

    Week 04 C++ Course

---
+++ Example_2D.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+
+char go='t';
+cout << "Are your friend going to university..."<<endl;
+cout <<"Press y for Yes OR press any letter for No :";
+cin>> go;
+if(go=='y')
+{
+cout<<"Now,I am going to university."<<endl;
+}
+else
+{
+cout<<"Now,I am not  going to university."<<endl;
+}
+cout<<"Program End.";
+}
