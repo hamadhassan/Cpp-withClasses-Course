b880e1cb6ea96eec6bee89dcc1c3f25091972c3b
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-25 17:25:38 +0500

    Week 04 C++ Course

--- Problem_6.cpp
+++ Problem_6.cpp
@@ -1,18 +1,29 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    /*Amir is a fat guy. He wants to lose weight but he is also weak in calculations. Write a simple C++ program
-    which tells him how many days he will need to lose weight if he strictly follows the doctor's suggestions.
-    Doctor suggested that if he eats 4000 calories daily, he walks for 1 hour daily. It will help him to lose 1 kg
-    weight after 15 days. Now Amir wants to know how many days he will need to lose 12 Kg weight if he
-    follows the doctor's suggestion.*/
-    int cal;
-    cout << "Amir eats 4000 calories,walk 1 hour daily and lose 1 kg weight in 15 days." << endl;
-    cal = 15 * 12;
-    cout << "Amir lose 12kg weight in " << cal << " Days.";
-    cout << endl
-         << "OR" << endl;
-    cal = cal / 30;
-    cout << "In " << cal << " Months";
+float speed;
+cout << "Enter Speed : ";
+cin>> speed;
+if(speed<=10)
+{
+cout <<"Slow";
+}
+else if(speed<=50)
+{
+cout <<"Avearge";
+}
+else if(speed<=150)
+{
+cout <<"Fast";
+}
+else if(speed<=1000)
+{
+cout <<"Ultra Fast";
+}
+else if(speed>1000)
+{
+cout <<"Extreme Fast";
+}
+cout <<endl<< "Thank for using.";
 }
