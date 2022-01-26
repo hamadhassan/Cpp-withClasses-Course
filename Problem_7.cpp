4548d5a6abdf230382d15697e62c8085338d273a
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-26 19:18:03 +0500

    Week 04 C++ Course

--- Problem_7.cpp
+++ Problem_7.cpp
@@ -1,23 +1,38 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
 int main()
 {
-    /*During each summer, Ahmad and Fatima grow vegetables in their backyard and buy seeds and fertilizer
-    from a local nursery. The nursery carries different types of vegetable fertilizers in various bag sizes. When
-    buying a particular fertilizer, they want to know the price of the fertilizer per pound and the cost of
-    fertilizing per square foot. Write the Algorithm that prompts the user to enter
-    1. the size of the fertilizer bag in pounds.
-    2. the cost of the bag.
-    3. and the area in square feet that can be covered by the bag.*/
-    int pound, cost, area = 0;
-    cout << "Enter size of the fertilizer bag in pounds:";
-    cin >> pound;
-    cout << "Enter the cost of the bag:";
-    cin >> cost;
-    cout << "Enter the area in square feet that can be covered by the bag:";
-    cin >> area;
-    cost = cost / pound;
-    area = area / cost;
-    cout << "The cost of the fertilizer per pound is:" << cost << "RS" << endl;
-    cout << "The cost of fertilizing the area per square foot is:" << area << "RS";
+string option;
+float area,side,width,radius,base,hight;
+cout << "Calculate the Area of geometry figure...";
+cin>>option;
+if(option=="square")
+{
+cout << "Enter the side of squre : ";
+cin>> side;
+area=side*side;
+}
+else if( option=="rectangule")
+{
+cout << "Enter the length of rectangule : ";
+cin>> side;
+cout << "Enter the width of rectangule  : ";
+cin>> width;
+area=width*side;
+}
+else if( option=="circle")
+{
+cout << "Enter the radius of circle : ";
+cin>> radius;
+area=radius*radius*2.14;
+}
+else if(option=="triangle")
+{
+cout<<"Enter the base of triangle : ";
+cin>>base;
+cout<<"Enter the hight of triangle :";
+cin>>hight;
+area=0.5*(base*hight);
+}
+cout<< "Area is :"<<area;
 }
