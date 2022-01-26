ccd7a4bed8cbaf8e662a6065bcbf4003b3da8fac
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-26 19:16:23 +0500

    Week 04 C++ Course

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -2,21 +2,12 @@
 using namespace std;
 int main()
 {
-string country;
-float price;
-cout << "Enter Country name : ";
-cin>> country;
-cout << "Enter Ticket pice : " ;
-cin>> price;
-if(country=="ireland")
-{
-price=price-(price*0.10);
-}
-else
-{
-price=price-(price*0.05);
-}
-cout << "Price after discount is : "<<price<<endl;
-cout << "Thanks for using.";
-
+float temp;
+cout<<"Enter the Temperature of Patient:";
+cin>> temp;
+if(temp == 98.6)
+ {
+  cout<< "Normal";
+ }
+cout<<"Program End";
 }
