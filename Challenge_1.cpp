fe80e49fa80171e37c5dab2571d4c96e3a3738d7
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-28 18:14:59 +0500

    Week 05 C++ Course

--- Challenge_1.cpp
+++ Challenge_1.cpp
@@ -2,15 +2,25 @@
 using namespace std;
 int main()
 {
-int n;
-cout<< "Enter a integer number : ";
-cin>> n;
-if(n%2==0)
-{
-cout<<"The number is even.";
-}
-else 
-{
-cout <<"The number is odd";
-}
+   int amount,dis,payable;
+   string day;
+   cout<< "Enter the Purchase Amount : ";
+   cin>> amount;
+   cout << "Enter the day : ";
+   cin>> day;
+   if(amount>=5000 && day=="sunday")
+   {
+       dis=amount*0.10;
+       payable=amount-dis;
+   }
+   else
+   {
+       dis=0;
+       payable=amount;
+   }
+   cout<< "------------------------"<<endl;
+   cout << "Discount is : "<< dis<<endl;
+   cout<< "Payable Amount : "<< payable<<endl;
+   cout<<"Thanks for using.";
+
 }
