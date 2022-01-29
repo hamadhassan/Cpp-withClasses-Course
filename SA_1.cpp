d9fa1b5026a5efaa6ead2d88a779b07f56e54b36
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-29 20:11:52 +0500

    Week 05 C++ Course

--- SA_1.cpp
+++ SA_1.cpp
@@ -1,18 +1,44 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-float gram,kg;
-cout << "Enter the grams : ";
-cin>> gram;
-if(gram<0)
-{
-cout << "You enter a wrong number";
-}
-else 
-{
-kg=(gram/1000);
-cout <<gram<< " grams equal to "<< kg<< " kilograms.";
-}
-cout <<endl<< "Program End";
+    int age1,age2,age3;
+    string name1,name2,name3;
+    cout << "Enter First brother name   : ";
+    cin>> name1;
+    cout << "Enter First brother age    : ";
+    cin>>age1;
+     cout << "Enter Second brother name : ";
+    cin>> name2;
+    cout << "Enter Second brother age   : ";
+    cin>>age2;
+     cout << "Enter Third brother name  : ";
+    cin>> name3;
+    cout << "Enter Third brother age    : ";
+    cin>>age3;
+    if(age1<age2)
+    {
+        if (age1<age3)
+        {
+            cout << "Younger brother is "<< name1;
+        }
+        else
+        {
+           cout << "Younger brother is "<< name3;
+        }
+    }
+    else
+    {
+        if(age2<age3)
+        {
+            cout << "Younger brother is "<< name2;
+        }
+        else
+        {
+             cout << "Younger brother is "<< name3;
+
+        }
+
+
+    }
 }
