1fe413632b10ac862a6a83dd198e938d621b3fe3
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-01-29 20:11:25 +0500

    Week 05 C++ Course

--- Challenge_4.cpp
+++ Challenge_4.cpp
@@ -2,15 +2,28 @@
 using namespace std;
 int main()
 {
-string pass;
-cout << "Enter the password : ";
-cin>> pass;
-if(pass=="123")
-{
-cout <<"Login Successfully.";
-}
-else 
-{
-cout << "Incorrect password";
-}
+   int amount,dis,payable;
+   amount=dis=payable=0;
+   string day,month;
+    day=month="abc";
+   cout<< "Enter the Purchase Amount : ";
+   cin>> amount;
+   cout<<"Enter the month : ";
+   cin>>month;
+   cout << "Enter the day : ";
+   cin>> day;
+   if((month=="october"|| month=="marach" || month=="august") &&( day=="sunday"))
+   {
+       dis=amount*0.10;
+       payable=amount-dis;
+   }
+   else if((month=="november"|| month=="december")&& (day=="monday"))
+   {
+      dis=amount*0.05;
+       payable=amount-dis;
+   }
+   cout<< "------------------------"<<endl;
+   cout << "Discount is : "<< dis<<endl;
+   cout<< "Payable Amount : "<< payable<<endl;
+   cout<<"Thanks for using.";
 }
