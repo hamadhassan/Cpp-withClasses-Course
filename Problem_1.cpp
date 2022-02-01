2089dd8d1e763041b93f508e6a73d9844a6b1ac1
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-01 16:41:22 +0500

    Week 05 C++ Course

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,13 +1,33 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-float temp;
-cout<<"Enter the Temperature of Patient:";
-cin>> temp;
-if(temp == 98.6)
- {
-  cout<< "Normal";
- }
-cout<<"Program End";
+    /* Write a C++ program to print the appropriate activity depending on the variable temperature and humidity
+    value. The table below assumes that the temperature can only be warm and cold, and the humidity can
+    only be dry and humid.*/
+
+    // Variable Deceleration
+    string temp, hum; //string data type varibale temperature, humidity
+    temp = hum = "";  // variable Inilizationa
+    cout << "Enter the temperatur: ";
+    cin >> temp;
+    cout << "Enter the humidity:";
+    cin >> hum;
+    // Conditions
+    if (temp == "warm" && hum == "dry")
+    {
+        cout << "Play Tennis";
+    }
+    else if (temp == "warm" && hum == "humid")
+    {
+        cout << "Swim";
+    }
+    else if (temp == "cold" && hum == "dry")
+    {
+        cout << "Play basketball";
+    }
+    else if (temp == "cold" && hum == "humid")
+    {
+        cout << "Watch tv";
+    }
 }
