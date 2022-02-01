e6f8a1e03531f4f84c2cd2c3e748110c1b8d6dd5
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-01 16:41:53 +0500

    Week 05 C++ Course

--- Problem_8.cpp
+++ Problem_8.cpp
@@ -2,525 +2,106 @@
 using namespace std;
 int main()
 {
-/* */
-#pragma region Variable Deceleration
+    /*A student has to attend an exam at a particular time (for example, at 9:30 am). They arrive in
+    the exam room at a particular time of arrival (for example 9:40 am). It is considered that the
+    student has arrived on time if they have arrived at the time when the exam starts or up to half
+    an hour earlier. If the student has arrived more than 30 minutes earlier, the student has come
+    too early. If they have arrived after the time when the exam starts, they are late.
+    Write a program that inputs the exam starting time and the time of student's arrival, and prints if the
+    student has arrived on time, if they have arrived early or if they are late, as well as how many hours or
+    minutes the student is early or late.
+    Input Data
+    Read the following four integers (one on each line) from the console:
+    The first line contains exam starting time (hours) – an integer from 0 to 23.
+    ● The second line contains exam starting time (minutes) – an integer from 0 to 59.
+    ● The third line contains an hour of arrival – an integer from 0 to 23.
+    ● The fourth line contains minutes of arrival – an integer from 0 to 59.
+    Output Data
+    Print the following on the first line on the console:
+    ● "Late", if the student arrives later compared to the exam starting time.
+    ● "On time", if the student arrives exactly at the exam starting time or up to 30 minutes earlier.
+    ● "Early", if the student arrives more than 30 minutes before the exam's starting time.
+    If the student arrives with more than one minute difference compared to the exam starting time, print on
+    the next line:
+    "mm minutes before the start" for arriving less than an hour earlier.
+    ● "hh:mm hours before the start" for arriving 1 hour or earlier. Always print minutes using 2
+    digits, for example "1:05".
+    ● "mm minutes after the start" for arriving more than an hour late.
+    ● "hh:mm hours after the start" for arriving late by 1 hour or more. Always print minutes using 2
+    digits, for example, "1:03". */
 
-    int n, mod, div;                                                   // user enter number, moddulus of number and divisible of number
-    string word, one, two, three, four, five, six, seven, eight, nine; // enter number in english word
-    cout << "Enter number between (0-100) : ";
-    cin >> n;
-    one = "one";
-    two = "two";
-    three = "three";
-    four = "four";
-    five = "five";
-    six = "six";
-    seven = "seven";
-    eight = "eight";
-    nine = "nine";
-#pragma endregion
+    int exam_hr, exam_mm, st_hr, st_mm, rem_hr, rem_mm, mm; // Exam Houre , Exam Minute , Student Houre, Student Minutes, remaning hour, remaning minutes, conert hours to minutes
+    // Input
+    cout << "Exam Starting Time (Hour) : ";
+    cin >> exam_hr;
+    cout << "Exam Starting Time (Minutes) : ";
+    cin >> exam_mm;
+    cout << "Student hour of arrival : ";
+    cin >> st_hr;
+    cout << "Student minutes of arrival : ";
+    cin >> st_mm;
+    // exam and student time match
+    if ((exam_hr == st_hr) && (exam_mm == st_mm))
+    {
+        cout << "On Time";
+    }
+    // exam and student time not match different possibility
+    else if ((exam_hr != st_hr && exam_mm != st_mm) || (exam_hr == st_hr && exam_mm != st_mm) || (exam_hr != st_hr && exam_mm == st_mm))
+    {
+        rem_hr = st_hr - exam_hr; // total hour
+        rem_mm = st_mm - exam_mm; // total minutes
+        if (rem_hr == 0)
+        {
+            if (rem_mm <= 30)
+            {
+                cout << "On Time" << endl;
+                cout << rem_mm << " minutes after the start.";
+            }
 
-#pragma region 1 to 19
-    if (n == 1)
-    {
-        word = one;
-    }
-    else if (n == 2)
-    {
-        word = two;
-    }
-    else if (n == 3)
-    {
-        word = three;
-    }
-    else if (n == 4)
-    {
-        word = four;
-    }
-    else if (n == 5)
-    {
-        word = five;
-    }
-    else if (n == 6)
-    {
-        word = six;
-    }
-    else if (n == 7)
-    {
-        word = seven;
-    }
-    else if (n == 8)
-    {
-        word = eight;
-    }
-    else if (n == 9)
-    {
-        word = nine;
-    }
-    else if (n == 10)
-    {
-        word = "ten ";
-    }
-    else if (n == 11)
-    {
-        word = "eleven";
-    }
-    else if (n == 12)
-    {
-        word = "twelve";
-    }
-    else if (n == 13)
-    {
-        word = "thirteen";
-    }
-    else if (n == 14)
-    {
+            else
+            {
+                cout << "Late" << endl;
+                cout << rem_mm << " minutes before the start.";
+            }
+        }
+        else if (rem_hr >= 0)
+        {
+            mm = rem_hr * 60;
+            mm = rem_mm + mm;
+            if (mm > 60)
+            {
+                cout << "Late" << endl;
+                cout << rem_hr << ":" << rem_mm << " after the start.";
+            }
+            else
+            {
+                cout << mm << " minutes after the start";
+            }
+        }
+        else if (rem_hr <= 0)
+        {
 
-        word = "fourteen";
-    }
-    else if (n == 15)
-    {
+            rem_hr = rem_hr * -1;
+            mm = rem_hr * 60;
+            rem_mm = rem_mm * -1;
+            mm = rem_mm + mm;
+            if (mm > 60)
+            {
+                cout << "Early" << endl;
+                cout << rem_hr << ":" << rem_mm << " before the start.";
+            }
+            else if (mm == 60)
+            {
+                cout << "Early " << endl;
+                cout << rem_hr << ":0 hour before the start.";
+            }
+            // less then 60
+            else
+            {
 
-        word = "fifteen";
-    }
-    else if (n == 16)
-    {
-
-        word = "sixteen";
-    }
-    else if (n == 17)
-    {
-
-        word = "seventeen";
-    }
-    else if (n == 18)
-    {
-
-        word = "eighteen";
-    }
-    else if (n == 19)
-    {
-
-        word = "ninteen";
-    }
-#pragma endregion
-
-#pragma region 20 to 29
-    // The nummber start from 20 and end at 29
-    else if (n >= 20 && n <= 29)
-    {
-
-        mod = n % 20;
-        div = n / 20;
-        word = "twenty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
+                cout << "One time " << endl;
+                cout << mm << " minutes before the start";
+            }
         }
     }
-#pragma endregion
-
-#pragma region 30 to 39
-
-    // The nummber start from 30 and end at 39
-    else if (n >= 30 && n <= 39)
-    {
-
-        mod = n % 30;
-        div = n / 30;
-        word = "thirty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 40 to 49
-    // The nummber start from 40 and end at 49
-    else if (n >= 40 && n <= 49)
-    {
-
-        mod = n % 40;
-        div = n / 40;
-        word = "fourty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 50 to 59
-    // The nummber start from 50 and end at 59
-    else if (n >= 50 && n <= 59)
-    {
-
-        mod = n % 50;
-        div = n / 50;
-        word = "fifty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 60 to 69
-    // The nummber start from 60 and end at 69
-    else if (n >= 60 && n <= 69)
-    {
-
-        mod = n % 60;
-        div = n / 60;
-        word = "sixty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 70 to 79
-    // The nummber start from 70 and end at 79
-    else if (n >= 70 && n <= 79)
-    {
-
-        mod = n % 70;
-        div = n / 70;
-        word = "seventy";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 80 to 89
-    // The nummber start from 80 and end at 89
-    else if (n >= 80 && n <= 89)
-    {
-
-        mod = n % 80;
-        div = n / 80;
-        word = "eighty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 90 to 99
-    // The nummber start from 90 and end at 99
-    else if (n >= 90 && n <= 99)
-    {
-
-        mod = n % 90;
-        div = n / 90;
-        word = "ninty";
-        if (mod == 0)
-        {
-            word;
-        }
-        else if (mod == 1)
-        {
-            word = word + one;
-        }
-        else if (mod == 2)
-        {
-            word = word + two;
-        }
-        else if (mod == 3)
-        {
-            word = word + three;
-        }
-        else if (mod == 4)
-        {
-            word = word + four;
-        }
-        else if (mod == 5)
-        {
-            word = word + five;
-        }
-        else if (mod == 6)
-        {
-            word = word + six;
-        }
-        else if (mod == 7)
-        {
-            word = word + seven;
-        }
-        else if (mod == 8)
-        {
-            word = word + eight;
-        }
-        else if (mod == 9)
-        {
-            word = word + nine;
-        }
-    }
-#pragma endregion
-
-#pragma region 100
-    else if (n == 100)
-    {
-        word = "one hunderd";
-    }
-
-    cout << "-------------------------" << endl;
-    cout << word;
-#pragma endregion
 }
