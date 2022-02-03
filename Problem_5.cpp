611e8d36d6609201be6d92bcd93063478b1c2a93
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-03 15:52:48 +0500

    Week 05 C++ Course

--- Problem_5.cpp
+++ Problem_5.cpp
@@ -1,28 +1,79 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
 {
-int holidays,wrkdays,playtime,timeleft,hur,mint;
-cout << "Enter the Holidays : ";
-cin>> holidays;
-wrkdays=365-holidays;
-playtime=(wrkdays*63+holidays*127);
-timeleft=(30000-playtime);
-hur=timeleft/60;
-mint=timeleft%60;
-if(playtime<=30000)
-{
-cout <<"Tom Sleep Well."<<endl;
-cout<<hur<<" Hours and "<< mint<< " minutes less for play.";
-}
-else 
-{
-cout <<"Tom will run away."<<endl;
-hur=(hur*(-1));
-mint=(mint*(-1));
-cout<<hur<<" Hours and "<< mint<< " minutes for play.";
-}
+    /*Write a program that calculates and prints the bill for a cellular telephone company. The company offers
+    two types of service: regular and premium. Its rates vary, depending on the type of service. The rates are
+    computed as follows:
+    Regular service: $10.00 plus first 50 minutes are free. Charges for over 50 minutes are $0.20
+    per minute.
+    Premium service: $25.00 plus:
+    ● For calls made during the day., the first 75 minutes are free; charges for more than 75 minutes are
+    $0.10 per minute.
+    ● For calls made during the night, the first 100 minutes are free; charges for more than 100 minutes
+    are $0.05 per minute.
+    Your program should prompt the user to enter a service code (type char), and the number of minutes the
+    service was used.
+    A service code of r or R means regular service; a service code of p or P
+    means premium service. Treat any other character as an error. Your program
+    should output the type of service, the number of minutes the telephone service was used, and the
+    amount due from the user.
+    For the premium service, the customer may be using the service during the
+    day and the night(d or D for day and n or N for night). Therefore, to calculate the bill, you must
+    ask the user to input the number of minutes the service was used during the day and the number
+    of minutes the service was used during the night. */
 
+    float minutes, overtime, charges;
+    char type, subType; // type means regular or perimum user, sub type means day or night user
+    cout << "Enter which type of pakage you used (Press R for regular and P for premium)...";
+    cin >> type;
+    cout << "Enter the minutes you used :";
+    cin>>minutes;
+    //conditions 
+    if (type == 'R' || type == 'r')
+    {
+        if (minutes > 50)
+        {
+            overtime = minutes - 50;
+            charges = overtime * 0.20;
+            charges = charges + 50;
+        }
+        else
+        {
+            charges = 50;
+        }
+    }
+    else if (type == 'P' || type == 'p')
+    {
+        cout << "Enter service using time (Press D for Day and N for Night)...";
+        cin >> subType;
+        if (subType == 'D' || subType == 'd')
+        {
+            overtime = minutes - 75;
+            charges = overtime * 0.10;
+            charges = charges + 75;
+        }
+        else
+        {
+            charges = 75;
+        }
 
-
+        if (subType == 'N' || subType == 'n')
+        {
+            overtime = minutes - 100;
+            charges = overtime * 0.05;
+            charges = charges + 100;
+        }
+        else
+        {
+            charges = 100;
+        }
+    }
+    else
+    {
+        cout << "Wrong input";
+    }
+    // Output 
+    cout << "--------------------------------------------" << endl;
+    cout << "The Service Charges is : " << charges;
 }
