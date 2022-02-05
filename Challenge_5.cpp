eee7ab9b982fa2c2f0db7ec6b94a362167bf8c41
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-05 22:41:58 +0500

    Week 06 C++ Course

--- Challenge_5.cpp
+++ Challenge_5.cpp
@@ -1,29 +1,76 @@
 #include <iostream>
 using namespace std;
+// Function prototype or deceleration
+string maximum(int firstnumber, int secondnumber, int thirdnumber, int fourthnumber); // Maximum number
+string minimum(int firstnumber, int secondnumber, int thirdnumber, int fourthnumber); // Minimum number
+// Main Function
 int main()
 {
-   int amount,dis,payable;
-   amount=dis=payable=0;
-   string day,month;
-   day=month="abc";
-   cout<< "Enter the Purchase Amount : ";
-   cin>> amount;
-   cout<<"Enter the month : ";
-   cin>>month;
-   cout << "Enter the day : ";
-   cin>> day;
-   if(month=="october" && day=="sunday")
-   {
-       dis=amount*0.10;
-       payable=amount-dis;
-   }
-   else if(month=="november" && day=="thursday")
-   {
-        dis=amount*0.10;
-       payable=amount-dis;
-   }
-   cout<< "------------------------"<<endl;
-   cout << "Discount is : "<< dis<<endl;
-   cout<< "Payable Amount : "<< payable<<endl;
-   cout<<"Thanks for using.";
+    /*Write two functions to print the maximum and the minimum number respectively among four
+    numbers entered by the user. */
+    int n1, n2, n3, n4;
+    string resultmax, resultmin;
+    cout << "Enter first number: ";
+    cin >> n1;
+    cout << "Enter second  number: ";
+    cin >> n2;
+    cout << "Enter third number: ";
+    cin >> n3;
+    cout << "Enter fourth number: ";
+    cin >> n4;
+    resultmax = maximum(n1, n2, n3, n4); // function called
+    cout << "------------------------" << endl;
+    cout << resultmax << endl;
+    cout << "------------------------" << endl;
+    resultmin = minimum(n1, n2, n3, n4); // function called
+    cout << resultmin;
+
+    
+}
+
+// Function definition
+// Maximum number
+string maximum(int firstnumber, int secondnumber, int thirdnumber, int fourthnumber)
+{
+    string result;
+    if ((firstnumber > secondnumber) && (firstnumber > thirdnumber) && (firstnumber > fourthnumber))
+    {
+        result = "Firt number is maximum then others ";
+    }
+    else if ((secondnumber > firstnumber) && (secondnumber > thirdnumber) && (secondnumber > fourthnumber))
+    {
+        result = "Second number is maximum then others ";
+    }
+    else if ((thirdnumber > firstnumber) && (thirdnumber > secondnumber) && (thirdnumber > fourthnumber))
+    {
+        result = "Third number is maximum then others ";
+    }
+    else
+    {
+        result = "Fourth number is maximum then others ";
+    }
+    return result;
+}
+
+// Mainimum number
+string minimum(int firstnumber, int secondnumber, int thirdnumber, int fourthnumber)
+{
+    string result;
+    if ((firstnumber < secondnumber) && (firstnumber < thirdnumber) && (firstnumber < fourthnumber))
+    {
+        result = " Firt number is minimum then others ";
+    }
+    else if ((secondnumber < firstnumber) && (secondnumber < thirdnumber) && (secondnumber < fourthnumber))
+    {
+        result = "Second number is minimum then others ";
+    }
+    else if ((thirdnumber < firstnumber) && (thirdnumber < secondnumber) && (thirdnumber < fourthnumber))
+    {
+        result = "Third number is minimum then others ";
+    }
+    else
+    {
+        result = "Fourth number is minimum then others ";
+    }
+    return result;
 }
