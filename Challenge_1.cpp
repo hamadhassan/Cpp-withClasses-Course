f8c1f4233a766a59b98abe3947ce3b3726f9ce81
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-11 08:11:22 +0500

    Week 07 C++ Course

--- Challenge_1.cpp
+++ Challenge_1.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
+// prototype
+void printTable(int table);
 int main()
 {
-    /*
-    The angle of elevation from a point 43 feet from the base of a tree on level ground to the
-    top of the tree is 30° (30 degree). Write a C++ program to calculate the height of the tree?*/
-    double angle_degree, base, deg_rad, height; // angle in the degree , base of triangle , convert degree into radian, height
-    cout << "Enter the angle of elevation : ";
-    cin >> angle_degree;
-    cout << "Enter the base of a tree : ";
-    cin >> base;
-    // convert degree into radian
-    deg_rad = angle_degree / 57.2958;
-    height = tan(deg_rad) * base;
-    // Height 
-    cout << "The height is : " << height;
+    // int table;
+    // cout << "Enter the table number : ";
+    // cin >> table;
+    printTable(24);
+    cout << "---------------" << endl;
+    printTable(50);
+    cout << "---------------" << endl;
+    printTable(29);
+    cout << "---------------" << endl;
+}
+// function deceleration
+void printTable(int table)
+{
+    for (int i = 1; i <= 10; i++)
+    {
+        cout << table << " * " << i << " = " << table * i << endl;
+    }
 }
