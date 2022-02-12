2d2aa63e2e33eb4c271ca884d4792f3525c40e14
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-12 12:19:23 +0500

    Week 07 C++ Course

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,33 +1,18 @@
 #include <iostream>
 using namespace std;
+/* [Display name ten times */
+// Function Prototype
+void name();
 int main()
 {
-    /* Write a C++ program to print the appropriate activity depending on the variable temperature and humidity
-    value. The table below assumes that the temperature can only be warm and cold, and the humidity can
-    only be dry and humid.*/
-
-    // Variable Deceleration
-    string temp, hum; //string data type varibale temperature, humidity
-    temp = hum = "";  // variable Inilizationa
-    cout << "Enter the temperatur: ";
-    cin >> temp;
-    cout << "Enter the humidity:";
-    cin >> hum;
-    // Conditions
-    if (temp == "warm" && hum == "dry")
+    cout << "Name is printed ten times." << endl;
+    name(); // function call
+}
+// Function Definition
+void name()
+{
+    for (int i = 1; i <= 10; i++)
     {
-        cout << "Play Tennis";
-    }
-    else if (temp == "warm" && hum == "humid")
-    {
-        cout << "Swim";
-    }
-    else if (temp == "cold" && hum == "dry")
-    {
-        cout << "Play basketball";
-    }
-    else if (temp == "cold" && hum == "humid")
-    {
-        cout << "Watch tv";
+        cout << "Name: Muhammad Hammad Hassan" << endl;
     }
 }
