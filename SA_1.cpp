90315265c658322dca934ef8ceecd9776d93dadd
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-14 14:13:41 +0500

    Week 07 C++ Course

--- SA_1.cpp
+++ SA_1.cpp
@@ -2,43 +2,18 @@
 using namespace std;
 int main()
 {
-    int age1,age2,age3;
-    string name1,name2,name3;
-    cout << "Enter First brother name   : ";
-    cin>> name1;
-    cout << "Enter First brother age    : ";
-    cin>>age1;
-     cout << "Enter Second brother name : ";
-    cin>> name2;
-    cout << "Enter Second brother age   : ";
-    cin>>age2;
-     cout << "Enter Third brother name  : ";
-    cin>> name3;
-    cout << "Enter Third brother age    : ";
-    cin>>age3;
-    if(age1<age2)
+    float average;
+    average = 0.0;
+    int n, num;
+    n = num = 0;
+    cout << "Enter the limit of taking number : ";
+    cin >> n; // limit of number
+    for (int i = 1; i <= n; i += 1)
     {
-        if (age1<age3)
-        {
-            cout << "Younger brother is "<< name1;
-        }
-        else
-        {
-           cout << "Younger brother is "<< name3;
-        }
+        cout << "Enter the number : ";
+        cin >> num; // store the user input
+        num += num;
     }
-    else
-    {
-        if(age2<age3)
-        {
-            cout << "Younger brother is "<< name2;
-        }
-        else
-        {
-             cout << "Younger brother is "<< name3;
-
-        }
-
-
-    }
+    average = num / n;
+    cout << "The average of " << n << " number is : " << average;
 }
