d4af86bb310d2d0eb131350256e7c3f0b836c8ab
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-14 14:12:52 +0500

    Week 07 C++ Course

--- Problem_3.cpp
+++ Problem_3.cpp
@@ -1,18 +1,25 @@
 #include <iostream>
 using namespace std;
-// prototype
-void number();
 
 int main()
 {
-    number(); // function call
-}
-// function decleration
-void number()
-{
-    for (int i = 1; i <= 100; i++)
+    int days, doctor, patient, trated, un_treated, totalcapacity, totalpatient;
+    doctor = 7;
+    cout << "Enter the days : ";
+    cin >> days;
+    for (int n = 1; n <= days; n++)
     {
-        cout << endl
-             << i;
+        cout << "Enter the patients : ";
+        cin >> patient;
+        if (n % 3 == 0)
+        {
+            if (un_treated > doctor)
+            {
+                totalcapacity = doctor + 1;
+            }
+        }
+        int rem = doctor - patient;
+        un_treated += rem;
+        totalpatient = totalpatient + patient;
     }
 }
