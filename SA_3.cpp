e808538ac7da611a87ffe37d21fecfa24ea4a4fd
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-14 14:15:10 +0500

    Week 07 C++ Course

--- SA_3.cpp
+++ SA_3.cpp
@@ -1,27 +1,32 @@
-#include<iostream>
+#include <iostream>
 using namespace std;
 int main()
+/*Write a C++ Program that asks the user to enter 5
+numbers, one at a time, and add them together. This
+is called a Running Total. Once the user is done,
+display the total sum on the Console.*/
 {
-int n1,n2,n3,n4,n5;
-float ave;
-cout<<"Enter first number : ";
-cin>>n1;
-cout<<"Enter second number : ";
-cin>>n2;
-cout<<"Enter third number : ";
-cin>>n3;
-cout<<"Enter fourth number : ";
-cin>>n4;
-cout<<"Enter fifth number : ";
-cin>>n5;
-ave=(n1+n2+n3+n4+n5)/5;
-if(ave<=70)
-{
-cout <<"Keep working hard. ";
-}
-else 
-{
-cout <<"You are brilliant. ";
-}
-cout<<endl<<"Program End";
+    int num, sum; // variable deceleration
+    num = sum = 0;
+    // for looop
+    /*for (int n = 0; n < 5; n++)
+    {
+        system("cls"); // clear screen
+        cout << "Enter a number : ";
+        cin >> num;
+        sum = sum + num;
+    }
+    cout << "Sum of five number is : " << sum;*/
+    // same question solved with while loop
+    int n;
+    n = 0; // intilization
+    while (n < 5)
+    {
+        system("cls"); // clear screen
+        cout << "Enter a number : ";
+        cin >> num;
+        sum = sum + num;
+        n += 1; // increment operator
+    }
+    cout << "Sum of five number is : " << sum;
 }
