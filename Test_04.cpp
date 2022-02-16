2b6911f5e17682f05a968db40b27f28be8ab9376
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-02-16 17:33:42 +0500

    Week 07 C++ Course

---
+++ Test_04.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    for (int i = 10; i >= 1; i--)
+    {
+        for (int n=10; n>=i;n--)
+        {
+            cout<<" ";
+        }
+        for(int j=1; j<=i;j++)
+        {
+            cout<<"*";
+        }
+        cout<<endl;
+
+    }
+}
