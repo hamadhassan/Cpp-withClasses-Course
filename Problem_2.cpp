4ae5bc70baed7471bdbc293534554d8434699b69
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-04 07:27:26 +0500

    Week 10 C++ Course

--- Problem_2.cpp
+++ Problem_2.cpp
@@ -1,23 +1,117 @@
 #include <iostream>
+#include <fstream>
+#include <string>    //for getline function
+#include <windows.h> //color the text
 using namespace std;
-// prototype
-int dots(int number);
+// Function Prototype
+void menuOption();
+bool checkUser(string User_username, string User_password);
+string seperateField(string record, int fieldNumber);
+void CreateUser();
+// Data Structure
+const int length = 10;
+string username[length];
+string password[length];
+int index = 0;
 int main()
 {
-    int sum, num;
-    sum = 0;
-    cout << "Enter the triangle : ";
-    cin >> num;
-    sum = dots(num);
-    cout << "Total triangle is : " << sum;
-}
-// deceleration
-int dots(int number)
-{
-    int sum = 0;
-    for (int n = 1; n <= number; n++)
+    string user;
+    string pass;
+    int option;
+    while (true)
     {
-        sum = sum + n;
+        cout << "------------------------------------------------------" << endl;
+        cout << "|                 Security Application               |" << endl;
+        cout << "------------------------------------------------------" << endl;
+        cout << "Choose Option:" << endl;
+        cout << " 1.LogIn" << endl;
+        cout << " 2.SignUp" << endl;
+        cout << " 3.Exit" << endl;
+        cout << "Press the number...";
+        cin >> option;
+        if (option == 1)
+        {
+            HANDLE h= GetStdHandle(STD_OUTPUT_HANDLE);
+            
+            cout << "Enter username : ";
+            cin >> user;
+            cout << "Enter password : ";
+            cin >> pass;
+            bool condition = checkUser(user, pass);
+            if (condition == true)
+            {
+                SetConsoleTextAttribute(h,2);
+                cout << "Access Granted" << endl;
+              // system("Color 2");
+            }
+            else if (condition == false)
+            {
+                cout << "Access Denied" << endl;
+                SetConsoleTextAttribute(h,4);
+              //  system("Color 4");
+
+            }
+        }
+        else if (option == 2)
+        {
+            CreateUser();
+        }
+        else if (option == 3)
+        {
+            break;
+        }
     }
-    return sum;
+}
+// Function Definition
+void CreateUser()
+{
+    string createUserName;
+    string createUserPassword;
+    cout << "Enter username : ";
+    cin >> createUserName;
+    cout << "Enter password : ";
+    cin >> createUserPassword;
+    fstream append;
+    append.open("Problem_2Data.txt", ios::app);
+    append << createUserName << "," << createUserPassword << "\n";
+    append.close();
+} // end of function
+bool checkUser(string User_username, string User_password)
+{
+    string record;
+    fstream read;
+    read.open("Problem_2Data.txt", ios::in);
+    while (!read.eof())
+    {
+        getline(read, record);
+        username[index] = seperateField(record, 1);
+        password[index] = seperateField(record, 2);
+        if (username[index] == User_username && password[index] == User_password)
+        {
+            index++;
+            return true;
+        }
+        else
+        {
+        }
+    }
+    index++;
+    return false;
+} // end of function
+string seperateField(string record, int fieldNumber)
+{
+    int count = 1;
+    string value;
+    for (int i = 0; i < record.length(); i++)
+    {
+        if (record[i] == ',')
+        {
+            count++;
+        }
+        else if (count == fieldNumber)
+        {
+            value = value + record[i];
+        }
+    }
+    return value;
 }
