84b57e1042c20e083226f74fa1d764fa357b8a31
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-04 07:26:08 +0500

    Week 10 C++ Course

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,29 +1,65 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-int main()
+string parseRecord(string record, int field);
+void inputData();
+void displayOutput(int index);
+string names[100], passwords[100];
+int ages[100];
+float cgpa[100];
+int idx = 0;
+main()
 {
-    int n = 4, k = 2;
-    cout << ++n << endl;
-     cout << n << endl;
-     cout << n++ << endl;
-     cout << n << endl;
-     cout << -n << endl;
-     cout << n << endl;
-     cout << --n << endl;
-     cout << n << endl;
-     cout << n-- << endl;
-     cout << n << endl;
-     cout << n + k << endl;
-     cout << n << endl;
-     cout << k << endl;
-     cout << n << k << endl;
-     cout << n << endl;
-     cout << " " << n << endl;
-     cout << " n" << endl;
-     cout << "\n"
-          << endl;
-     cout << " n * n = "; // CAREFUL!
-     cout << n * n << endl;
-     cout << 'n' << endl;
-    return 0;
+    inputData();
+    string n, p;
+    cout << "Enter Name: ";
+    cin >> n;
+    cout << "Enter Password: ";
+    cin >> p;
+    for (int x = 0; x < idx; x++)
+    {
+        if (n == names[x] && p == passwords[x])
+        {
+            displayOutput(x);
+        }
+    }
+}
+string parseRecord(string record, int field)
+{
+    int commaCount = 1;
+    string item;
+    for (int x = 0; x < record.length(); x++)
+    {
+        if (record[x] == ',')
+        {
+            commaCount = commaCount + 1;
+        }
+        else if (commaCount == field)
+        {
+            item = item + record[x];
+        }
+    }
+    return item;
+}
+void inputData()
+{
+    string word;
+    fstream f_variable;
+    f_variable.open("Data.txt", ios::in);
+    while (!(f_variable.eof()))
+    {
+        getline(f_variable, word);
+        names[idx] = parseRecord(word, 1);
+        passwords[idx] = parseRecord(word, 2);
+        ages[idx] = stoi(parseRecord(word, 3));
+        cgpa[idx] = stof(parseRecord(word, 4));
+        idx = idx + 1;
+    }
+}
+void displayOutput(int index)
+{
+    cout << "Name \t Age \t CGPA" << endl;
+
+    cout << names[index] << " \t " << ages[index];
+    cout << " \t " << cgpa[index] << endl;
 }
