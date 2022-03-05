0302ede2e2923a447a73da33cc03b3e915a82f28
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-05 18:29:33 +0500

    Week 10 C++ Course

--- Problem1.cpp
+++ Problem1.cpp
@@ -1,65 +1,20 @@
 #include <iostream>
+#include <conio.h>
 #include <fstream>
 using namespace std;
-string parseRecord(string record, int field);
-void inputData();
-void displayOutput(int index);
-string names[100], passwords[100];
-int ages[100];
-float cgpa[100];
-int idx = 0;
-main()
+int main()
 {
-    inputData();
-    string n, p;
-    cout << "Enter Name: ";
-    cin >> n;
-    cout << "Enter Password: ";
-    cin >> p;
-    for (int x = 0; x < idx; x++)
-    {
-        if (n == names[x] && p == passwords[x])
-        {
-            displayOutput(x);
-        }
-    }
-}
-string parseRecord(string record, int field)
-{
-    int commaCount = 1;
-    string item;
-    for (int x = 0; x < record.length(); x++)
-    {
-        if (record[x] == ',')
-        {
-            commaCount = commaCount + 1;
-        }
-        else if (commaCount == field)
-        {
-            item = item + record[x];
-        }
-    }
-    return item;
-}
-void inputData()
-{
-    string word;
-    fstream f_variable;
-    f_variable.open("Data.txt", ios::in);
-    while (!(f_variable.eof()))
-    {
-        getline(f_variable, word);
-        names[idx] = parseRecord(word, 1);
-        passwords[idx] = parseRecord(word, 2);
-        ages[idx] = stoi(parseRecord(word, 3));
-        cgpa[idx] = stof(parseRecord(word, 4));
-        idx = idx + 1;
-    }
-}
-void displayOutput(int index)
-{
-    cout << "Name \t Age \t CGPA" << endl;
-
-    cout << names[index] << " \t " << ages[index];
-    cout << " \t " << cgpa[index] << endl;
+    int distance, time, speed;
+    cout << "Enter the distance : ";
+    cin >> distance;
+    cout << "Enter the time : ";
+    cin >> time;
+    speed = distance / time;
+    fstream newFile;
+    newFile.open("problem1_data.txt", ios::out); // write mode
+    newFile << "Distance is : " << distance << "\n";
+    newFile << "Time  is : " << time << "\n";
+    newFile << "Speed is : " << speed << "\n";
+    cout << "Speed is : " << speed;
+    newFile.close();
 }
