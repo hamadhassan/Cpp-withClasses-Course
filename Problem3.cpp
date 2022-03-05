86ceed3aebca47d0b8730ef91f90bba74a4c2b02
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-05 18:30:47 +0500

    Week 10 C++ Course

--- Problem3.cpp
+++ Problem3.cpp
@@ -1,17 +1,31 @@
 #include <iostream>
+#include <fstream>
+#include <conio.h>
 using namespace std;
 int main()
 {
-    int n;
-    cout << (n = 4) << endl;
-    cout << (n == 4) << endl;
-    cout << (n > 3) << endl;
-    cout << (n < 4) << endl;
-    cout << (n = 0) << endl;
-    cout << (n == 0) << endl;
-    cout << (n > 0) << endl;
-    cout << (n && 4) << endl;
-    cout << (n || 4) << endl;
-    cout << (!n) << endl;
-    return 0;
+    int distance, time, speed;
+    cout << "Enter the distance : ";
+    cin >> distance;
+    cout << "Enter the time : ";
+    cin >> time;
+    speed = distance / time;
+    cout << "Speed is : " << speed;
+    fstream write;
+    write.open("problem3_data.txt", ios::out);
+    write << "Distance is : " << distance << "\n";
+    write << "Time is : " << time << "\n";
+    write << "Speed is : " << speed << "\n";
+    write.close();
+    string distance1, time1, speed1;
+    fstream read;
+    read.open("problem3_data.txt", ios::in);
+    getline(read,distance1);
+    getline(read,time1);
+    getline(read,speed1);
+    read.close();
+    cout<<endl;
+    cout << distance1 << endl;
+    cout << time1 << endl;
+    cout << speed1 << endl;
 }
