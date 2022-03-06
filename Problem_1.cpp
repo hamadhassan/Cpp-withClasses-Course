821db994b3e082de6314ec811624b62492a57f5e
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-06 06:36:59 +0500

    Week 10 C++ Course

--- Problem_1.cpp
+++ Problem_1.cpp
@@ -1,24 +1,73 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-void amplify();
+void getBirthdayCake(string name, int age);     // write data
+void readData();                                // read data
+string sepreatedData(string record, int field); // read the comma sperated file and return the field block
+void DisplayData();                             // display data according to the formate
+string names;
+int ages;
 int main()
 {
-    amplify();
+    string name;
+    int age;
+    cout << "Enter name : ";
+    cin >> name;
+    cout << "Enter age : ";
+    cin >> age;
+    getBirthdayCake(name, age);
+    readData();
+    DisplayData();
 }
-void amplify()
+void getBirthdayCake(string name, int age)
 {
-
-    int num, multiply, sequence;
-    multiply = 1;
-    num = 25;
-    for (int n = 1; n <= num; n++)
+    fstream write;
+    write.open("Problem_1Data.txt", ios::out);
+    write << name << "," << age;
+    write.close();
+} // end of data write
+void readData()
+{
+    string record;
+    fstream read;
+    read.open("Problem_1Data.txt", ios::in);
+    while (!(read.eof()))
     {
-        sequence = n;
-
-        if (sequence % 4 == 0)
+        getline(read, record);
+        names = sepreatedData(record, 1);
+        ages = stoi(sepreatedData(record, 2));
+    }
+    read.close();
+}
+string sepreatedData(string record, int field)
+{
+    int count = 1;
+    string value;
+    for (int i = 0; i < record.length(); i++)
+    {
+        if (record[i] == ',')
         {
-            multiply = sequence * 10;
-            cout << " " << multiply;
+            count++;
         }
+        else if (count == field)
+        {
+            value = value + record[i];
+        }
+    }
+    return value;
+}
+void DisplayData()
+{
+    if (ages % 2 == 0)
+    {
+        cout << "###############" << endl;
+        cout << "# " << ages << " HB " << names << "! " << ages << " #" << endl;
+        cout << "###############" << endl;
+    }
+    else
+    {
+        cout << "***************" << endl;
+        cout << "* " << ages << " HB " << names << "! " << ages << " *" << endl;
+        cout << "***************" << endl;
     }
 }
