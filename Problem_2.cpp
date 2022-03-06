ea94e662d69545c608b27e0a2b1b8f414a0a3436
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-06 06:37:38 +0500

    Week 10 C++ Course

--- Problem_2.cpp
+++ Problem_2.cpp
@@ -1,117 +1,33 @@
 #include <iostream>
 #include <fstream>
-#include <string>    //for getline function
-#include <windows.h> //color the text
 using namespace std;
-// Function Prototype
-void menuOption();
-bool checkUser(string User_username, string User_password);
-string seperateField(string record, int fieldNumber);
-void CreateUser();
-// Data Structure
-const int length = 10;
-string username[length];
-string password[length];
-int index = 0;
+void readData();
 int main()
 {
-    string user;
-    string pass;
-    int option;
-    while (true)
-    {
-        cout << "------------------------------------------------------" << endl;
-        cout << "|                 Security Application               |" << endl;
-        cout << "------------------------------------------------------" << endl;
-        cout << "Choose Option:" << endl;
-        cout << " 1.LogIn" << endl;
-        cout << " 2.SignUp" << endl;
-        cout << " 3.Exit" << endl;
-        cout << "Press the number...";
-        cin >> option;
-        if (option == 1)
-        {
-            HANDLE h= GetStdHandle(STD_OUTPUT_HANDLE);
-            
-            cout << "Enter username : ";
-            cin >> user;
-            cout << "Enter password : ";
-            cin >> pass;
-            bool condition = checkUser(user, pass);
-            if (condition == true)
-            {
-                SetConsoleTextAttribute(h,2);
-                cout << "Access Granted" << endl;
-              // system("Color 2");
-            }
-            else if (condition == false)
-            {
-                cout << "Access Denied" << endl;
-                SetConsoleTextAttribute(h,4);
-              //  system("Color 4");
-
-            }
-        }
-        else if (option == 2)
-        {
-            CreateUser();
-        }
-        else if (option == 3)
-        {
-            break;
-        }
-    }
+    readData();
 }
-// Function Definition
-void CreateUser()
+void readData()
 {
-    string createUserName;
-    string createUserPassword;
-    cout << "Enter username : ";
-    cin >> createUserName;
-    cout << "Enter password : ";
-    cin >> createUserPassword;
-    fstream append;
-    append.open("Problem_2Data.txt", ios::app);
-    append << createUserName << "," << createUserPassword << "\n";
-    append.close();
-} // end of function
-bool checkUser(string User_username, string User_password)
-{
-    string record;
+    string alp;
     fstream read;
     read.open("Problem_2Data.txt", ios::in);
-    while (!read.eof())
+    getline(read, alp);
+    int count = 0;
+    char loop = 'a';
+    while (loop <= 'z')
     {
-        getline(read, record);
-        username[index] = seperateField(record, 1);
-        password[index] = seperateField(record, 2);
-        if (username[index] == User_username && password[index] == User_password)
+        fstream append;
+        if (alp[count] == loop)
         {
-            index++;
-            return true;
         }
         else
         {
+            append.open("Problem_2Data.txt", ios::app);
+            append << loop;
         }
+        loop++;
+        count++;
+        append.close();
     }
-    index++;
-    return false;
-} // end of function
-string seperateField(string record, int fieldNumber)
-{
-    int count = 1;
-    string value;
-    for (int i = 0; i < record.length(); i++)
-    {
-        if (record[i] == ',')
-        {
-            count++;
-        }
-        else if (count == fieldNumber)
-        {
-            value = value + record[i];
-        }
-    }
-    return value;
+    read.close();
 }
