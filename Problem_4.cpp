4784fb50244483f750c1e7bcf7c47192e4efdd1e
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-07 14:55:45 +0500

    Week 10 C++ Course

--- Problem_4.cpp
+++ Problem_4.cpp
@@ -1,57 +1,20 @@
 #include <iostream>
+#include <fstream>
 using namespace std;
-// Function prototype or deceleration
-string leapYear(int year);
-// Main Function
+void readData();
+
 int main()
 {
-    /* Create a function that takes 2 years as input, the difference between the years will be exactly 10. For
-    example if the first input is the year 2005 then the next input will be 2015. Your goal is to count how
-    many leap years there were between these 10 years and return that count from the function.
-    Test Cases:
-    countLeapYears(2010, 2020) ➞ 3
-    countLeapYears(2016, 2026) ➞ 3
-    countLeapYears(2005, 2015) ➞ 2
-    */
-    int year_1, year_2, counter;
-    string result_1, result_2;
-
-    cout << "Enter a year : ";
-    cin >> year_1;
-    cout << "Enter a year : ";
-    cin >> year_2;
-    result_1 = leapYear(year_1); // function called
-    result_2 = leapYear(year_2); // function called
-    if (result_1 == " is leap year" || result_2 == " is leap year")
-    {
-        counter = 3;
-    }
-    else
-    {
-        counter = 2;
-    }
-    cout << counter << " leap yaer between them.";
+    readData();
 }
-// Function definition
-string leapYear(int year)
-{ // function body
-    string result;
-    if (year % 4 == 0 && year % 100 == 0 && year % 400 == 0)
+void readData()
+{
+    string record;
+    fstream read;
+    read.open("Problem_4Data.txt", ios::in);
+    while (!(read.eof()))
     {
-        result = " is leap year";
+        getline(read, record);
+        cout << record;
     }
-    else if (year % 4 == 0 && year % 100 != 0)
-    {
-        result = " is leap year";
-    }
-    else if (year % 4 != 0)
-    {
-        result = " not leap year";
-    }
-    else if (year % 4 == 0 && year % 100 == 0 && year % 400 != 0)
-    {
-        result = " not leap year";
-    }
-    return result;
-    return result;
 }
