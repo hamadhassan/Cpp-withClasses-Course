089db42bcf3650af98f38dbb1cac8b164b88493c
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-10 10:31:17 +0500

    Week 10 C++ Course

---
+++ Problem11.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n = -3;
+    if (n < 10 && n > 0)
+    {
+
+        cout << "The number is positive." << endl;
+    }
+    else
+    {
+        cout << "The number is negative." << endl;
+    }
+}
