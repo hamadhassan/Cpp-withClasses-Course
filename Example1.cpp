49fcac1241ebbf4c40adf57ed711a1094689a964
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-18 06:55:29 +0500

    Week 12 C++ Course

--- Example1.cpp
+++ Example1.cpp
@@ -1,17 +1,9 @@
 #include <iostream>
 using namespace std;
-/*Write a program that initialize matrix named as cars mentioned in Figure-1 and 
-display values.*/
+/*Write a program that shows address of a variable using & operator.*/
 int main()
 {
-    const int length = 5;
-    int cars[length][length] = {{10, 7, 12, 10, 4}, {18, 11, 15, 17, 2}, {23, 19, 12, 16, 14}, {7, 12, 16, 0, 2}, {3, 5, 6, 2, 1}};
-    for (int x = 0; x < length; x++)
-    {
-        for (int y = 0; y < length; y++)
-        {
-             cout << cars[x][y] << "\t";
-        }
-        cout << endl;
-    }
+    int a = 5;
+    cout << "Variable value : " << a << endl;
+    cout << "Variable address : " << &a;
 }
