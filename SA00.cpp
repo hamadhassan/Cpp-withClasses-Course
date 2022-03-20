45a685eb970aa9cc2660b629460f0b2b8aea5807
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-20 10:30:44 +0500

    Week 12 C++ Course

---
+++ SA00.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+void swapThemByVal(int num1, int num2);
+void swapThemByRef(int &num1, int &num2);
+int main()
+{
+    int i = 10, j = 20;
+    swapThemByVal(i, j);
+    cout << i << " " << j << endl;
+    swapThemByRef(i, j);
+    cout << i << " " << j << endl;
+}
+void swapThemByVal(int num1, int num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
+void swapThemByRef(int &num1, int &num2)
+{
+    int temp = num1;
+    num1 = num2;
+    num2 = temp;
+}
