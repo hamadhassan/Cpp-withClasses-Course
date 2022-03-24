d423d69ff6513cf5dea152c5d72fc01774237102
Muhammad Hammad Hassan <96945594+hamadhassan@users.noreply.github.com>
2022-03-24 11:44:09 +0500

    Week 12 C++ Course

--- Example1.cpp
+++ Example1.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
 using namespace std;
-/*Write a program that shows address of a variable using & operator.*/
 int main()
 {
-    int a = 5;
-    cout << "Variable value : " << a << endl;
-    cout << "Variable address : " << &a;
+    int a[10]={10};
+    for (int x = 0; x < 10; x++)
+    {
+        cout << a[x] << endl;
+    }
 }
